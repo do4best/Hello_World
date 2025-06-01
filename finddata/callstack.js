60dbc32e0e05e05466d6395cfebd30fd985426ab
Meer Afzal <do4best@gmail.com>
2025-06-01 21:51:30 +0500

    try to recall some function need to be sure

---
+++ finddata/callstack.js
@@ -0,0 +1,20 @@
+function one() {
+    console.log("one")
+
+}
+function two() {
+    console.log("two")
+  
+}
+function three() {
+    console.log("three")
+
+}
+function four() {
+    console.log("four")
+   
+}
+one()
+two()
+three()
+four()
---
+++ finddata/index.js
@@ -0,0 +1,11 @@
+const changColor=()=>`#${Math.floor(Math.random()* 0xfffff).toString(16).padEnd(6,'0')}`
+const checkNumger = (num) => num % 2 === 0
+const findAverage = (...args) => args.reduce((a, b) => a + b) / args.length;
+const shaffleArray=(arr)=>arr.sort(()=>0.5 - Math.random())
+let arr=[2,3,4,5,6,7,8,9]
+document.querySelector("#app").innerHTML = `
+<h1 style='color:${changColor()}'>Hello</h1>
+<h1 style='color:${changColor()}'>(${checkNumger(15) ? "is Even" : "is odd"})</h1>
+<h1 style='color:${changColor()}'>The rresult is ${findAverage(1, 2, 3, 4, 5, 6, 7, 8, 9)}</h1>
+<h1 style='color:${changColor()}'>The Random Arrays are : ${shaffleArray(arr)}</h1
+`
