afa24f0449b49d709125c1768af9621edb316581
Meer Afzal <do4best@gmail.com>
2025-06-18 23:10:31 +0500

    Some Arrow function is exercised such as remove an element from array and get the day name while inserting a date in digits

--- finddata/oneArrowfunction1.js
+++ finddata/oneArrowfunction1.js
@@ -15,6 +15,12 @@ const remainingMinute = (min) => {
     const reemaingMinute = min % 60
     return `The is ${hour} hours and minute are ${reemaingMinute}`
 }
+// remove a specific element in array
+const removeElement = (arr, ele) => arr.filter(e => e !== ele);
+const returnDay = (day) => {
+    const dayofWeek = [ "Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    return dayofWeek[day.getDay()]
+}
 const movies = ['speed','night is hunted','beatle']
 const findIndex=(arr,value)=>arr.indexOf(value)
 const findLeapYear=(year)=>(year%4 === 0 && year % 100 !== 0) || (year%400 === 0)
@@ -32,3 +38,5 @@ console.log(arraytoscv([1, 2, 3, 4, 5, 6]))
 console.log(findLeapYear(2028))
 console.log(findIndex(movies, 'beatle'))
 console.log(remainingMinute(700))
+console.log(removeElement(movies, 'speed '))
+console.log(returnDay(new Date("6/28/2025")))
