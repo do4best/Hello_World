ba8340cb85374bf3179bd457cc54cded02644198
Meer Afzal <do4best@gmail.com>
2025-07-18 00:15:17 +0500

    try to find the recursion way

--- finddata/understandingObject.js
+++ finddata/understandingObject.js
@@ -16,3 +16,43 @@ function creatingPrivate(name,job) {
 let myJob = new creatingPrivate("Sane", "SE",)
 myJob.setJob("Plumber")
 console.log(myJob.getJob())
+// understanding recursion
+function callitself(n) {
+    if (n < 0) {
+        return;
+    } else {
+        console.log(n)
+        callitself(n-1)
+    }
+}
+function letme(n) {
+ 
+    for (let i = 0; i < n; i++){
+       console.log(i)
+    }
+ 
+}
+letme(10)
+// callitself(9)
+// iterative version
+function getFibo(n) {
+    if (n <= 0) return;
+    let sum = 0;
+    let last = 1;
+    let lastlast = 0;
+    for (let i = 0; i < n; i++){
+        sum = lastlast + last;
+        lastlast = last;
+        last = sum
+    }
+    return sum;
+}
+console.log(getFibo(10))
+function fabniacchi(n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        return fabniacchi(n-1)+fabniacchi(n-2)
+    }
+}
+console.log(fabniacchi(2))
